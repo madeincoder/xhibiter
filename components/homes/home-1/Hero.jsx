e425e7d47ed69535eb58c15d4724b11df321b688
madeincoder <ctgseafood@gmail.com>
2025-02-04 17:19:38 +0600

    webm format video uploaded

--- components/homes/home-1/Hero.jsx
+++ components/homes/home-1/Hero.jsx
@@ -81,23 +81,23 @@ export default function Hero() {
                 </g>
               </svg> */}
               <div className="relative rounded-3xl">
-                {/* <video
-                  src="/img/hero/video/hero-video.mp4"
+                <video
+                  src="/img/video/hero-video.webm"
                   autoPlay
                   loop
                   muted
                   className="relative mt-0 inline-block w-full"
                   clipPath="url(#clipping)"
-                ></video> */}
-                <iframe
-                  src="https://player.vimeo.com/video/1052989665?autoplay=1&loop=1&muted=1&background=1"
+                ></video>
+                {/* <iframe
+                  src="https://player.vimeo.com/video/1053327742?autoplay=1&loop=1&muted=1"
                   width="100%"
                   height="500"
                   allow="autoplay; fullscreen; picture-in-picture"
                   allowFullScreen
                   loading="lazy"
                   className="relative mt-0 inline-block w-full"
-                />
+                /> */}
               </div>
               {/* <Image
                   width={740}
